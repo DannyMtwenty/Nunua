ec0661fc9a66c8f2d8913ed4420bbccc3aa1383b
DannyMtwenty <dannywanyoike@gmail.com>
2022-07-18 00:08:05 +0300

    state,, addd and subtratct ,,,counter

--- nunua/App.js
+++ nunua/App.js
@@ -1,22 +1,61 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 
 class NunuaApp extends Component {
-constructor(props){
-
-  this.state={
-    value : 0,
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+    };
   }
-}
+
+  subtract = () => {
+    let initialValue = this.state.value;
+    this.setState({
+      value: initialValue-1,
+    });
+  };
+
+  add = () => {
+    let initialValue = this.state.value;
+    this.setState({
+      value: initialValue + 1,
+    });
+  };
 
   render() {
     return (
-      <View style={{
+      <View
+        style={{
           flex: 1,
           justifyContent: "center",
-          alignItems: "center"
+          alignItems: "center",
         }}>
-        <Text>Hello, world!</Text>
+        <Text>{this.state.value}</Text>
+
+        <TouchableOpacity
+          onPress={this.subtract}
+          style={{
+            backgroundColor: "red",
+            padding: 10,
+            margin: 10,
+            height: 50,
+            width: 100,
+          }}>
+          <Text>-</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={this.add}
+          style={{
+            backgroundColor: "red",
+            padding: 10,
+            margin: 10,
+            height: 50,
+            width: 100,
+          }}>
+          <Text>+</Text>
+        </TouchableOpacity>
       </View>
     );
   }
