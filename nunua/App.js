e612e5233f28372f105dd510051a6f998f0984a0
DannyMtwenty <dannywanyoike@gmail.com>
2022-07-20 22:59:42 +0300

    more styleing

--- nunua/App.js
+++ nunua/App.js
@@ -22,7 +22,7 @@ class NunuaApp extends Component {
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          marginTop: 10,
+          marginTop: 30,
         }}>
         <View
           style={{
@@ -53,7 +53,6 @@ class NunuaApp extends Component {
               height: 50 + "%",
               justifyContent: "flex-start",
               paddingLeft: 10,
-              
             }}>
             <Text>Search here</Text>
           </View>
@@ -63,53 +62,113 @@ class NunuaApp extends Component {
           style={{
             flex: 2,
             borderWidth: 4,
-            borderColor: "green",
+            borderColor: "yellow",
             flexDirection: "row",
             width: 100 + "%",
-            marginRight : 15
+            marginRight: 15,
+            justifyContent: "flex-start",
+            alignItems: "center",
+            paddingLeft: 10,
           }}>
           <View
-           style={{
-            flex: 2,
-            borderWidth: 2,
-            backgroundColor: "red",
-            flexDirection: "row",
-            width: 20 + "%",
-            height: 50 + "%",
-            borderRadius: 15,
-            marginRight : 15
-          }}
-          >
-            <Text>Groceries</Text>
+            style={{
+              borderWidth: 2,
+              backgroundColor: "grey",
+
+              width: 20 + "%",
+              height: 50 + "%",
+
+              borderRadius: 15,
+              marginRight: 15,
+            }}>
+            <View
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                height: 65 + "%",
+                borderColor: "red",
+                borderWidth: 2,
+              }}>
+              <Text>IC</Text>
+            </View>
+
+            <View
+              style={{
+                borderColor: "red",
+                justifyContent: "center",
+                alignItems: "center",
+                height: 35 + "%",
+              }}>
+              <Text>Groceries</Text>
+            </View>
+
           </View>
 
           <View
-           style={{
-            flex: 2,
-            borderWidth: 2,
-            backgroundColor: "red",
-            flexDirection: "row",
-            width: 20 + "%",
-            height: 50 + "%",
-            borderRadius: 15,
-            marginRight : 15
-          }}
-          >
-            <Text>Repair</Text>
+            style={{
+              borderWidth: 2,
+              backgroundColor: "grey",
+
+              width: 20 + "%",
+              height: 50 + "%",
+
+              borderRadius: 15,
+              marginRight: 15,
+            }}>
+            <View
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                height: 65 + "%",
+                borderColor: "red",
+                borderWidth: 2,
+              }}>
+              <Text>IC</Text>
+            </View>
+
+            <View
+              style={{
+                borderColor: "red",
+                justifyContent: "center",
+                alignItems: "center",
+                height: 35 + "%",
+              }}>
+              <Text>Repair</Text>
+            </View>
           </View>
 
           <View
-           style={{
-            flex: 2,
-            borderWidth: 2,
-            backgroundColor: "red",
-            flexDirection: "row",
-            width: 20 + "%",
-            height: 50 + "%",
-            borderRadius: 15,
-          }}
-          >
-            <Text>Gas delivery</Text>
+            style={{
+              borderWidth: 2,
+              backgroundColor: "grey",
+
+              width: 20 + "%",
+              height: 50 + "%",
+
+              borderRadius: 15,
+              marginRight: 15,
+            }}>
+            <View
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                height: 65 + "%",
+                borderColor: "red",
+                borderWidth: 2,
+              }}>
+              <Text>IC</Text>
+            </View>
+
+            <View
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+
+                height: 35 + "%",
+
+              }}>
+              <Text style={{fontSize:10}}>Gas delivery</Text>
+            </View>
           </View>
         </View>
 
